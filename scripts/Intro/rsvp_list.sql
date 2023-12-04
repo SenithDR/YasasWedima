d313a4ea128625bf23509d5ee149b89c45d64f4f
Senith Deelaka Ranchagoda <44867466+SenithDR@users.noreply.github.com>
2023-12-04 08:14:21 +0530

    sql file remove

--- scripts/Intro/rsvp_list.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO puraqryp_yasaswedima.rsvp_list(InviID, lastNme, firstNme, seats, contact, tableNo, s_confirmed, guestDetails, attending) VALUES (1, 'Ranchagoda', 'Senith', 4, '0712188098', 5, 0, '', 'No');
-INSERT INTO puraqryp_yasaswedima.rsvp_list(InviID, lastNme, firstNme, seats, contact, tableNo, s_confirmed, guestDetails, attending) VALUES (2, 'Hehe', 'Haha', 2, '121', 2, 0, null, 'Yes');
-INSERT INTO puraqryp_yasaswedima.rsvp_list(InviID, lastNme, firstNme, seats, contact, tableNo, s_confirmed, guestDetails, attending) VALUES (3, 'Sri', 'Yasas', 2, '0722222222', 1, 0, null, 'N/A');
